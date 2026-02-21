e9fdd3f9416cfb58586c3c67f56c8e8272dcf36f
Berk Geveci <berk.geveci@kitware.com>
2026-02-20 20:58:38 -0500

    Add volume rendering to sinusoid example to match book Figure 6-6

--- chapter06/examples/image_sinusoid.py
+++ chapter06/examples/image_sinusoid.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
-"""Create a sinusoidal image volume using vtkImageSinusoidSource."""
+"""Create a sinusoidal volume, cast to unsigned char, and volume render it."""
 
 import os
 import sys
 
-from vtkmodules.vtkImagingSources import vtkImageSinusoidSource
+from vtkmodules.vtkCommonDataModel import vtkPiecewiseFunction
 from vtkmodules.vtkFiltersModeling import vtkOutlineFilter
+from vtkmodules.vtkImagingCore import vtkImageCast
+from vtkmodules.vtkImagingSources import vtkImageSinusoidSource
 from vtkmodules.vtkRenderingCore import (
     vtkActor,
+    vtkColorTransferFunction,
     vtkPolyDataMapper,
     vtkRenderer,
     vtkRenderWindow,
     vtkRenderWindowInteractor,
+    vtkVolume,
+    vtkVolumeProperty,
 )
+from vtkmodules.vtkRenderingVolume import vtkFixedPointVolumeRayCastMapper
 import vtkmodules.vtkRenderingOpenGL2  # noqa: F401
+import vtkmodules.vtkRenderingVolumeOpenGL2  # noqa: F401
 
 # Create a sinusoidal source
 sinusoid = vtkImageSinusoidSource()
@@ -22,9 +29,41 @@ sinusoid.SetAmplitude(63)
 sinusoid.SetDirection(1, 0, 0)
 sinusoid.SetPeriod(25)
 
-# Create an outline
+# Cast from double to unsigned char for volume rendering
+cast = vtkImageCast()
+cast.SetInputConnection(sinusoid.GetOutputPort())
+cast.SetOutputScalarTypeToUnsignedChar()
+cast.ClampOverflowOn()
+
+# Volume rendering
+mapper = vtkFixedPointVolumeRayCastMapper()
+mapper.SetInputConnection(cast.GetOutputPort())
+
+# Transfer functions
+color_tf = vtkColorTransferFunction()
+color_tf.AddRGBPoint(0, 0.0, 0.0, 0.0)
+color_tf.AddRGBPoint(64, 1.0, 0.0, 0.0)
+color_tf.AddRGBPoint(128, 1.0, 1.0, 0.0)
+color_tf.AddRGBPoint(255, 1.0, 1.0, 1.0)
+
+opacity_tf = vtkPiecewiseFunction()
+opacity_tf.AddPoint(0, 0.0)
+opacity_tf.AddPoint(64, 0.2)
+opacity_tf.AddPoint(128, 0.5)
+opacity_tf.AddPoint(255, 1.0)
+
+volume_property = vtkVolumeProperty()
+volume_property.SetColor(color_tf)
+volume_property.SetScalarOpacity(opacity_tf)
+volume_property.ShadeOff()
+
+volume = vtkVolume()
+volume.SetMapper(mapper)
+volume.SetProperty(volume_property)
+
+# Outline bounding box
 outline = vtkOutlineFilter()
-outline.SetInputConnection(sinusoid.GetOutputPort())
+outline.SetInputConnection(cast.GetOutputPort())
 
 outline_mapper = vtkPolyDataMapper()
 outline_mapper.SetInputConnection(outline.GetOutputPort())
@@ -35,10 +74,12 @@ outline_actor.SetMapper(outline_mapper)
 # Rendering
 renderer = vtkRenderer()
 render_window = vtkRenderWindow()
+render_window.SetSize(400, 400)
 render_window.AddRenderer(renderer)
 interactor = vtkRenderWindowInteractor()
 interactor.SetRenderWindow(render_window)
 
+renderer.AddVolume(volume)
 renderer.AddActor(outline_actor)
 renderer.SetBackground(0.1, 0.2, 0.4)
 renderer.ResetCamera()
